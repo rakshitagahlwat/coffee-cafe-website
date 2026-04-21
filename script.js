88f12c717aeb2f8ae18e637a761bb8b55a51ea94
Rakshita Gahlawat <rakshitakumar2006@email.com>
2026-04-21 16:19:12 +0530

    Initial commit - Coffee Cafe Website

---
+++ script.js
@@ -0,0 +1,7 @@
+function openForm() {
+    document.getElementById("formPopup").style.display = "flex";
+}
+
+function closeForm() {
+    document.getElementById("formPopup").style.display = "none";
+}
